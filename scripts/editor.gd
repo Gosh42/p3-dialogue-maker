a4fb4d1798e199f35545ea4d93fc71e7bcb4eed5
Gosh42 <78163102+Gosh42@users.noreply.github.com>
2025-01-01 19:10:40 +0300

    corner colour picker + more day times

--- scripts/editor.gd
+++ scripts/editor.gd
@@ -5,6 +5,9 @@ extends Control
 @onready var screen: Screen = %Screen
 @onready var dialogue_edit: TextEdit = %DialogueEdit
 @onready var time_select: OptionButton = %TimeSelection
+@onready var day_colour_select: OptionButton = %DayColourSelection
+
+var custom_colour: Color = Color.WHITE
 
 
 func _ready() -> void:
@@ -17,6 +20,7 @@ func _on_screenshot_btn_button_down() -> void:
 	var image: Image = viewport_texture.texture.get_image()
 	image.save_png("res://images/yeah.png")
 
+# =================== NAME AND DIALOGUE ====================== #
 
 func _on_name_edit_text_changed(new_text: String) -> void:
 	screen.set_name_text(new_text)
@@ -25,6 +29,26 @@ func _on_name_edit_text_changed(new_text: String) -> void:
 func _on_text_edit_text_changed() -> void:
 	screen.set_dialogue_text(dialogue_edit.text)
 
+func _on_arrow_check_toggled(toggled_on: bool) -> void:
+	screen.toggle_arrow(toggled_on)
+
+# =================== TIME SELECTION ====================== #
 
 func _on_time_selected(index: int) -> void:
 	screen.set_daytime(index)
+
+# =================== COLOUR SELECTION ====================== #
+
+func _on_day_colour_selected(index: int) -> void:
+	# last always will be custom. this is a bad way to do this but i don't care
+	if index < day_colour_select.item_count - 1:
+		screen.choose_colour(index)
+	else:
+		screen.set_custom_colour(custom_colour)
+
+
+func _on_day_colour_picked(colour: Color) -> void:
+	custom_colour = colour
+	
+	if day_colour_select.selected == day_colour_select.item_count - 1:
+		screen.set_custom_colour(custom_colour)
--- scripts/screen.gd
+++ scripts/screen.gd
@@ -4,26 +4,63 @@ extends Control
 @onready var name_label: Label = %NameLabel
 @onready var dialogue_label: Label = %DialogueLabel
 @onready var corner: TextureRect = %Main
+@onready var arrow: TextureRect = %Arrow
 @onready var daytime_sprite: Sprite2D = %DaytimeSprite
 
 const CORNER_BLUE: Color = Color(0x4996feff)
+const CORNER_BLUE_PORTABLE: Color = Color(0x67bbffff)
+const CORNER_PINK: Color = Color(0xf898b3ff)
 const CORNER_GREEN: Color = Color(0x79fe8cff)
+const CORNER_GREEN_PORTABLE: Color = Color(0x2eb05aff)
+var current_colour: Color
 
 enum Daytimes {
-	EARLY_MORNING,	MORNING,
-	DAYTIME,		LUNCHTIME,
-	AFTERNOON,		AFTER_SCHOOL,
-	EVENING,		LATE_NIGHT,
-	DARK_HOUR
+	BEFORE_DAWN,
+	EARLY_MORNING,
+	MORNING,
+	DAYTIME,
+	LUNCHTIME,
+	AFTERNOON,
+	AFTER_SCHOOL,
+	EVENING,
+	LATE_NIGHT,
+	DARK_HOUR,
+	UNKNOWN
 }
 
 
-func set_name_text(str: String) -> void:
-	name_label.text = str
+func _ready() -> void:
+	current_colour = CORNER_BLUE
+
+# =================== NAME AND DIALOGUE ====================== #
+
+func set_name_text(text: String) -> void:
+	name_label.text = text
 
 
-func set_dialogue_text(str: String) -> void:
-	dialogue_label.text = str
+func set_dialogue_text(text: String) -> void:
+	dialogue_label.text = text
+
+# =================== COLOUR SELECTION ====================== #
+
+func choose_colour(index: int) -> void:
+	match index:
+		0: current_colour = CORNER_BLUE
+		1: current_colour = CORNER_BLUE_PORTABLE
+		2: current_colour = CORNER_PINK
+		3: current_colour = CORNER_GREEN
+		4: current_colour = CORNER_GREEN_PORTABLE
+	
+	change_corner_colour(current_colour)
+
+
+func set_custom_colour(colour: Color) -> void:
+	current_colour = Color(colour)
+	change_corner_colour(current_colour)
+
+
+func change_corner_colour(colour: Color) -> void:
+	corner.modulate = colour
 
 
 func set_daytime(index: int) -> void:
@@ -32,6 +69,10 @@ func set_daytime(index: int) -> void:
 	daytime_sprite.frame = daytime
 	
 	if daytime == Daytimes.DARK_HOUR:
-		corner.modulate = CORNER_GREEN
+		change_corner_colour(CORNER_GREEN)
 	else:
-		corner.modulate = CORNER_BLUE
+		change_corner_colour(current_colour)
+
+
+func toggle_arrow(toggled_on: bool) -> void:
+	arrow.visible = toggled_on
